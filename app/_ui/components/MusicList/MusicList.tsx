bbc7d4249f33a990290821122b49e598aad02355
juan-soares <juan.css013@gmail.com>
2024-07-04 20:28:31 -0300

    selected music

--- app/_ui/components/MusicList/MusicList.tsx
+++ app/_ui/components/MusicList/MusicList.tsx
@@ -1,15 +1,27 @@
+import { IMusic } from "@/app/_lib/interfaces";
 import styles from "./MusicList.module.css";
 
-export function MusicList() {
+interface IProps {
+  musics: IMusic[];
+  selectedMusic: IMusic;
+}
+
+export function MusicList({ musics, selectedMusic }: IProps) {
+  function isSelected(musicTitle: string) {
+    return selectedMusic.title === musicTitle ? styles["selected"] : "";
+  }
+
   return (
     <ul className={styles.musicList}>
-      <li className={styles.musicListItem}>
-        <div className={styles.listItemInformation}>
-          <span className={styles.musicTitle}></span>
-          <span className={styles.musicDuration}>00:00</span>
-          <span className={styles.musicArtist}>Art</span>
-        </div>
-      </li>
+      {musics.map(({ title, duration, artist }) => (
+        <li className={[styles["musicListItem"], isSelected(title)].join(" ")}>
+          <div className={styles.listItemInformation}>
+            <span className={styles.musicTitle}>{title}</span>
+            <span className={styles.musicDuration}>{duration}</span>
+            <span className={styles.musicArtist}>{artist}</span>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { MusicList } from "./_ui/components/MusicList";
 
+const musics = [{ id: "1", title: "a", duration: "0:00", artist: "adle" }];
+
 export default function Home() {
   return (
     <div>
-      <MusicList />
+      <MusicList musics={musics} selectedMusic={musics[0]} />
     </div>
   );
 }
